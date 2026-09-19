b55dab2bae126c22f5aa98a6c61f6c4b8ac60065
RSVK-21 <rshanmukhavinaykumar@gmail.com>
2026-09-19 10:25:25 +0530

    Create design.v

---
+++ full_adder/2_half_adders/design.v
@@ -0,0 +1,17 @@
+module half_adder(
+  input a,b, 
+  output sum, carry
+);
+  assign sum = a ^ b;
+  assign carry = a & b;
+endmodule
+
+module full_adder(
+  input a,b,cin, 
+  output sum, cout
+);
+  wire s1, c1, c2;
+  half_adder ha1(a,b,s1,c1);
+  half_adder ha2(s1,cin,sum,c2);
+  assign cout = c1 | c2;
+endmodule
